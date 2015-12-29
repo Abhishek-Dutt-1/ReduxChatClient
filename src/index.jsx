45d279f8368f33fd3b64851abee268cb9356da49
Abhishek Dutt <abhishekdutt@outlook.com>
2015-12-29 22:14:51 +0530

    Minor

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router, {Route} from 'react-router';
+import {List, Map} from 'immutable';
 import {createStore} from 'redux';
 import reducer from './reducer';
 import App from './components/App';
-import CommentBox from './components/CommentBox';
+import {CommentBoxContainer} from './components/example';
+import {Provider} from 'react-redux';
 
 const store = createStore(reducer);
 
 store.dispatch({
-
   type: 'SET_STATE',
   state: {
-    commentList: [
+    commentList: [ 
       {
         "id": 1,
         "author": "Batman",
@@ -21,18 +22,19 @@ store.dispatch({
       {
         "id": 2,
         "author": "Aquaman",
-        "text": "Aquaman Aquaman."
+        "text": "I am Aquaman."
       }
     ] // End commentList
   }   // End state
-
 });
 
 const routes = <Route component={App}>
-  <Route path="/" component={CommentBox}  />
+  <Route path="/" component={CommentBoxContainer} />
 </Route>;
 
 ReactDOM.render(
-  <Router>{routes}</Router>,
+  <Provider store={store}>
+    <Router>{routes}</Router>
+  </Provider>,
   document.getElementById('app')
 );
